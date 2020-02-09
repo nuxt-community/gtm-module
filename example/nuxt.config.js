390f4b97239de7699d71465f31f808f4196ee9b2
Manfies <okunevden97@gmail.com>
2020-02-10 00:31:25 +0300

    test: update tests for different modes and scripts existing

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -1,6 +1,7 @@
 const { resolve } = require('path')
 
 module.exports = {
+  mode: 'spa',
   rootDir: resolve(__dirname, '..'),
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
--- test/module.test.js
+++ test/module.test.js
@@ -1,18 +1,99 @@
-const { setup, loadConfig, get } = require('@nuxtjs/module-test-utils')
+const path = require('path')
+const { setup, loadConfig, get, url } = require('@nuxtjs/module-test-utils')
+const { defaultSettings } = require(path.join(__dirname, '../', 'lib', 'module.js'))
 
-describe('module', () => {
+describe('Module (universal mode)', () => {
   let nuxt
 
+  const nuxtConfig = loadConfig(__dirname, '../../example')
+  nuxtConfig.mode = 'universal'
+
+  const scriptId = nuxtConfig.gtm.scriptId || defaultSettings.scriptId
+  const noscriptId = nuxtConfig.gtm.noscriptId || defaultSettings.noscriptId
+
   beforeAll(async () => {
-    ({ nuxt } = (await setup(loadConfig(__dirname, '../../example'))))
+    ({ nuxt } = (await setup(nuxtConfig)))
   }, 60000)
 
   afterAll(async () => {
     await nuxt.close()
   })
 
-  test('render', async () => {
+  test('Render', async () => {
     const html = await get('/')
     expect(html).toContain('Works!')
   })
+
+  test('Has GTM script', async () => {
+    const html = await get('/')
+    expect(html).toContain(`data-hid="${scriptId}"`)
+  })
+
+  test('Hasn\'t GTM noscript', async () => {
+    const html = await get('/')
+    expect(html).not.toContain(`data-hid="${noscriptId}"`)
+  })
+
+  // test with real GTM id
+  const gtmId = nuxtConfig.gtm.id
+  if (gtmId !== 'XXXXXXX') {
+    test('GTM should be defined ($nuxt.$gtm)', async () => {
+      const window = await nuxt.renderAndGetWindow(url('/'))
+      expect(window.$nuxt.$gtm).toBeDefined()
+    })
+    // test('Verifying duplicate GTM script', async () => {
+    //   const window = await nuxt.renderAndGetWindow(url('/'))
+    //   const html = window.document.documentElement.innerHTML
+    //   console.log(html)
+    //   expect(html).toContain('Works!')
+    // })
+  }
+})
+
+describe('Module (spa mode)', () => {
+  let nuxt
+
+  const nuxtConfig = loadConfig(__dirname, '../../example')
+  nuxtConfig.mode = 'spa'
+
+  const scriptId = nuxtConfig.gtm.scriptId || defaultSettings.scriptId
+  const noscriptId = nuxtConfig.gtm.noscriptId || defaultSettings.noscriptId
+
+  beforeAll(async () => {
+    ({ nuxt } = (await setup(nuxtConfig)))
+  }, 60000)
+
+  afterAll(async () => {
+    await nuxt.close()
+  })
+
+  test('Render', async () => {
+    const html = await get('/')
+    expect(html).toContain('Loading...')
+  })
+
+  test('Has GTM script', async () => {
+    const html = await get('/')
+    expect(html).toContain(`data-hid="${scriptId}"`)
+  })
+
+  test('Hasn\'t GTM noscript', async () => {
+    const html = await get('/')
+    expect(html).not.toContain(`data-hid="${noscriptId}"`)
+  })
+
+  // test with real GTM id
+  const gtmId = nuxtConfig.gtm.id
+  if (gtmId !== 'XXXXXXX') {
+    test('GTM should be defined ($nuxt.$gtm)', async () => {
+      const window = await nuxt.renderAndGetWindow(url('/'))
+      expect(window.$nuxt.$gtm).toBeDefined()
+    })
+    // test('Verifying duplicate GTM script', async () => {
+    //   const window = await nuxt.renderAndGetWindow(url('/'))
+    //   const html = window.document.documentElement.innerHTML
+    //   console.log(html)
+    //   expect(html).toContain('Works!')
+    // })
+  }
 })
