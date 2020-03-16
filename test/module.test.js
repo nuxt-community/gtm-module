a251bf7417e1dc00e1f7fded9623c5f761db68aa
Rafał Chłodnicki <rchl2k@gmail.com>
2020-03-16 13:18:19 +0100

    fix: take into account router.base when reporting page tracking event
     - include router's base in `pageUrl` property of triggered page tracking events
     - replace head function in example nuxt.config.js with a plain object
       as that has triggered a noisy warning and wasn't tested anyway
     - add @types/jest to provide completions for test functions (when using
       relevant language server).

--- test/module.test.js
+++ test/module.test.js
@@ -2,6 +2,21 @@ const path = require('path')
 const { setup, loadConfig, get, url } = require('@nuxtjs/module-test-utils')
 const defaultSettings = require(path.join(__dirname, '../', 'lib', 'defaults.js'))
 
+function expectPageTrackingEvent (eventsArray, expectedEvent) {
+  return new Promise((resolve) => {
+    // Need to wait at least 250ms as that's how long plugin delays before triggering event.
+    setTimeout(() => {
+      expect(eventsArray).toStrictEqual(
+        expect.arrayContaining([
+          expect.objectContaining(expectedEvent)
+        ])
+      )
+      expect(eventsArray.filter(event => event.event === 'nuxtRoute').length).toBe(1)
+      resolve()
+    }, 300)
+  })
+}
+
 const modes = ['universal', 'spa']
 
 for (const mode of modes) {
@@ -53,6 +68,67 @@ for (const mode of modes) {
       const totalAmoutOfGtmScriptsAtHead = headGtmScriptsExternal.length + headGtmScriptsHid.length
 
       expect(totalAmoutOfGtmScriptsAtHead).toBeLessThan(3)
+    })
+
+    test('Should include pushed event', async () => {
+      const window = await nuxt.renderAndGetWindow(url('/'))
+      expect(window.dataLayer).toStrictEqual(
+        expect.arrayContaining([
+          expect.objectContaining({ event: 'ssr' })
+        ])
+      )
+      expect(window.dataLayer.filter(event => event.event === 'ssr').length).toBe(1)
+    })
+
+    test('Should include page tracking event', async () => {
+      const window = await nuxt.renderAndGetWindow(url('/'))
+
+      await expectPageTrackingEvent(window.dataLayer, {
+        event: 'nuxtRoute',
+        pageTitle: '@nuxtjs/gtm-module',
+        pageType: 'PageView',
+        pageUrl: '/',
+        routeName: 'index'
+      })
+    })
+  })
+}
+
+for (const mode of modes) {
+  describe(`Page tracking with router base (${mode} mode)`, () => {
+    let nuxt
+
+    const override = {
+      gtm: {
+        layer: 'testDataLayer',
+        pageTracking: true
+      }
+    }
+
+    const nuxtConfig = loadConfig(__dirname, '../../example', override, { merge: true })
+    if (!nuxtConfig.router) {
+      nuxtConfig.router = {}
+    }
+    nuxtConfig.router.base = '/base/'
+
+    beforeAll(async () => {
+      ({ nuxt } = (await setup(nuxtConfig)))
+    }, 60000)
+
+    afterAll(async () => {
+      await nuxt.close()
+    })
+
+    test('Event should contain page URL with base', async () => {
+      const window = await nuxt.renderAndGetWindow(url('/base/'))
+
+      await expectPageTrackingEvent(window.testDataLayer, {
+        event: 'nuxtRoute',
+        pageTitle: '@nuxtjs/gtm-module',
+        pageType: 'PageView',
+        pageUrl: '/base/',
+        routeName: 'index'
+      })
     })
   })
 }
